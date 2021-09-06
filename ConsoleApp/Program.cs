549fb04e22263bcd14d29a67b9d333f521f50a6b
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2021-09-06 16:20:05 +0200

    Create User Interface for IsLeapYear

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,10 @@ namespace ConsoleApp
 {
     public class Program {
         public static void Main(string[] args) {
-
+            while (true) {
+                var userInput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Number.IsLeapYear(userInput) ? "yay" : "nay");
+            }
         }
     }
 }
