013903aaa95926e8225e9df00bfce1d0957bc933
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2021-09-06 16:57:57 +0200

    Make Number class static

--- ConsoleApp/Number.cs
+++ ConsoleApp/Number.cs
@@ -1,5 +1,5 @@
 namespace ConsoleApp {
-    public class Number {
+    public static class Number {
         public static bool IsLeapYear(int year) {
             if (year % 400 == 0) return true;
             if (year % 100 == 0) return false;
